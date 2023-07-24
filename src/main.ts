0c78067e6764009297e96f7647943fc12ac207a4
mohammedaneesuddin <139983071+mohammedaneesuddin@users.noreply.github.com>
2023-07-24 13:32:56 +0530

    1 file modified

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,7 @@ import 'zone.js/dist/zone';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import 'hammerjs';
+
 
 import { platformBrowser } from '@angular/platform-browser';
 
