024dd6397f467a516e8d4cfbc361d38c6efbe2dc
mohammedaneesuddin <139983071+mohammedaneesuddin@users.noreply.github.com>
2023-07-26 11:12:33 +0530

    3 files modified

--- src/app/temp/temp.component.ts
+++ src/app/temp/temp.component.ts
@@ -1,3 +1,47 @@
+// import { Component, OnInit } from '@angular/core';
+// import { TempService } from '../services/temp.service';
+// import { MlProductList, PizzaDetail } from '../mlproductlist';
+
+// @Component({
+//   selector: 'app-temp',
+//   templateUrl: './temp.component.html',
+//   styleUrls: ['./temp.component.css']
+// })
+// export class TempComponent implements OnInit {
+//   public products: MlProductList[];  
+//   selectedSize: string | null = null;
+//   selectedPrice: number = 0;
+
+//   constructor(private srvCart: TempService) { }
+
+//   ngOnInit() {  
+//     this.products = this.srvCart.getproductlist();    
+//   } 
+
+//   calculatePrice() {
+//     if (this.selectedSize) {
+//       const selectedPizza = this.products.find(product => product.pizzaDetails.some((detail: PizzaDetail) => detail.pizzaSize === this.selectedSize));
+//       if (selectedPizza) {
+//         const pizzaDetail = selectedPizza.pizzaDetails.find((detail: PizzaDetail) => detail.pizzaSize === this.selectedSize);
+//         if (pizzaDetail) {
+//           this.selectedPrice = pizzaDetail.pizzaPrice;
+//         }
+//       }
+//     }
+//   }
+
+//   addToCart() {
+//     if (this.selectedSize) {
+//       // Here, you can add the selected pizza with the chosen size to the cart.
+//       // For example, you can pass the selectedPizza and selectedSize to the cart service or store them in an array representing the cart.
+//       console.log('Selected Pizza:', this.selectedSize);
+//     }
+//   }
+// }
+
+
+
+
 import { Component, OnInit } from '@angular/core';
 import { TempService } from '../services/temp.service';
 import { MlProductList, PizzaDetail } from '../mlproductlist';
@@ -9,6 +53,7 @@ import { MlProductList, PizzaDetail } from '../mlproductlist';
 })
 export class TempComponent implements OnInit {
   public products: MlProductList[];  
+  selectedPizza: MlProductList | null = null;
   selectedSize: string | null = null;
   selectedPrice: number = 0;
 
@@ -19,22 +64,11 @@ export class TempComponent implements OnInit {
   } 
 
   calculatePrice() {
-    if (this.selectedSize) {
-      const selectedPizza = this.products.find(product => product.pizzaDetails.some((detail: PizzaDetail) => detail.pizzaSize === this.selectedSize));
-      if (selectedPizza) {
-        const pizzaDetail = selectedPizza.pizzaDetails.find((detail: PizzaDetail) => detail.pizzaSize === this.selectedSize);
-        if (pizzaDetail) {
-          this.selectedPrice = pizzaDetail.pizzaPrice;
-        }
+    if (this.selectedPizza && this.selectedSize) {
+      const pizzaDetail = this.selectedPizza.pizzaDetails.find((detail: PizzaDetail) => detail.pizzaSize === this.selectedSize);
+      if (pizzaDetail) {
+        this.selectedPrice = pizzaDetail.pizzaPrice;
       }
-    }
-  }
-
-  addToCart() {
-    if (this.selectedSize) {
-      // Here, you can add the selected pizza with the chosen size to the cart.
-      // For example, you can pass the selectedPizza and selectedSize to the cart service or store them in an array representing the cart.
-      console.log('Selected Pizza:', this.selectedSize);
     }
   }
 }
